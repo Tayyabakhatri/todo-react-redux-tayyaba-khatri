b81d1b5ebd960eb7febbc224fa30798b8002a956
Tayyabakhatri <Tayyabakhatri225@gmail.com>
2025-01-23 09:48:37 +0500

    tailwind installed

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Tasks from './Components/Tasks.jsx'
+import './App.css'
+
+
+function App() {
+
+  return (
+    <>
+     <Tasks/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Tasks.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "tailwindcss";
+const Tasks = () => {
+  return (
+    <>
+    <div className='bg-[white] p-2 rounded'>
+<p>tayyaba</p>
+    </div>
+   
+    </>
+  )
+}
+
+export default Tasks
