412b91f75d80bfff9d7170dc9cbf8e9f794009c5
Tayyabakhatri <Tayyabakhatri225@gmail.com>
2025-01-30 23:35:24 +0500

    .

--- src/Components/Tasks.jsx
+++ src/Components/Tasks.jsx
@@ -183,7 +183,7 @@ const Tasks = () => {
         </div>
         {/* div 2 */}
         <div className="col-span-2 border-8">
-          <p>Make it heppen</p>
+          <p className=" font-mono text-center text-3xl">Your works to do</p>
           <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
             {tasks.map((task, index) => (
               <div
